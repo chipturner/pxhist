72ab9f26f0db7b4926b5e1dacd334c6c10982baf
chipturner <chipturner@users.noreply.github.com>
2026-03-26 22:47:51 +0000

    deploy: ecd7b8b71d4fee98c451962533248596a6e23b1f

--- docs/trait.impl/core/ops/drop/trait.Drop.js
+++ docs/trait.impl/core/ops/drop/trait.Drop.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["pxh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.0/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"pxh/recall/tui/struct.RecallTui.html\" title=\"struct pxh::recall::tui::RecallTui\">RecallTui</a>",0]]]]);
+    const implementors = Object.fromEntries([["pxh",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/ops/drop/trait.Drop.html\" title=\"trait core::ops::drop::Drop\">Drop</a> for <a class=\"struct\" href=\"pxh/recall/tui/struct.RecallTui.html\" title=\"struct pxh::recall::tui::RecallTui\">RecallTui</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
